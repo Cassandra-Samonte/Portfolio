5ee11aa8de3828e8d6f639470ea434f0b08d37c2
cassamonte <142133887+cassamonte@users.noreply.github.com>
2023-09-07 14:23:42 -0700

    added contact button

---
+++ app.js
@@ -0,0 +1,13 @@
+new fullpage('#fullpage', {
+    autoScrolling: true,
+    navigation: true,
+    scrollingSpeed: 1000
+ });
+
+ let typed = new Typed('.auto-type', {
+    strings: ['Hello World'],
+    typeSpeed: 150,
+    backSpeed:50,
+    startDelay: 10,
+    loop: true
+})
